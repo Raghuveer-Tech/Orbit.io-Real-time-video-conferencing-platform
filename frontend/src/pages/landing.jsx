b826cb98368b5e5cc2c83e745da71889e2939e98
Raghuveer-Tech <raghuveerkumawat.work@gmail.com>
2026-07-27 15:40:50 +0530

    add new image

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -120,7 +120,7 @@ export default function LandingPage() {
 
                 <div className="hero-right-media">
                     <div className="video-image-frame">
-                        <img src="/mobile.png" className="hero-call-image" alt="Orbit Video Call Platform Screen" />
+                        <img src="/mobile.jpg" className="hero-call-image" alt="Orbit Video Call Platform Screen" />
                         <div className="video-status-tag">
                             <span className="status-dot"></span> Connect Live Video Platform
                         </div>
